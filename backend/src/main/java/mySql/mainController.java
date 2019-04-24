4aa8d1e4a2a73e853aa42c9bcd6f5cd086cc39e0
Boo <boominghui1994@gmail.com>
2019-04-24 14:28:47 +0800

    Added mySql package, which contains user, user repo and controller class

---
+++ backend/src/main/java/mySql/mainController.java
@@ -0,0 +1,14 @@
+package mySql;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import mySql.User;
+import mySql.UserRepository;
+
+public class mainController {
+}
