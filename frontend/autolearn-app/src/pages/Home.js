d3e40fe499b23a7c4eda2ee9b522faa1128d0ca9
Allyson Ooi <allysonooi28@gmail.com>
2019-04-24 15:21:22 +0800

    Setup home page (temp)

--- frontend/autolearn-app/src/pages/Home.js
+++ frontend/autolearn-app/src/pages/Home.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
+import '../home.css';
+
+import { Container, Row, Col } from 'react-bootstrap';
+import { InputGroup, FormControl, Button } from 'react-bootstrap';
+import logo from '../logo.svg';
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                Home
+          <div>
+            <div className="Home-hero-div">
+              <Container>
+                <Row className="align-center">
+                    <div className="Home-search-bar">
+                      <InputGroup size="lg">
+                        <InputGroup.Prepend>
+                          <Button variant="secondary" className="Home-search-bar-btn">Search</Button>
+                        </InputGroup.Prepend>
+                        <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
+                      </InputGroup>
+                    </div>
+                </Row>
+              </Container>
             </div>
-        );
+            <div>
+              <Container>
+                <Row>
+                  <Col>1 of 3</Col>
+                  <Col>2 of 3</Col>
+                  <Col>3 of 3</Col>
+                </Row>
+              </Container>
+            </div>
+          </div>
+        )
     }
 }
 
 Home.propTypes = {
-    
+
 };
