06b8a9c6c36a06a3b6fa121ee97e3a7a8b947c0b
Angps1995 <angpengseng@hotmail.com>
2019-04-24 12:15:17 +0800

    added description

--- backend/src/models/Path.java
+++ backend/src/models/Path.java
@@ -6,6 +6,7 @@ public class Path {
     private String id;
     private String name;
     private List<Course> courses;
+    private String description;
     private int votes;
     private Topic topic;
 }
