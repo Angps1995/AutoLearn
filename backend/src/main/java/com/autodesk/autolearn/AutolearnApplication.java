0174013018516c954486dbb57d0e6134f70593ab
personal <getsong13@gmail.com>
2019-04-24 11:09:04 +0800

    Add initialized spring project in folder "backend"

---
+++ backend/src/main/java/com/autodesk/autolearn/AutolearnApplication.java
@@ -0,0 +1,13 @@
+package com.autodesk.autolearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutolearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutolearnApplication.class, args);
+	}
+
+}
