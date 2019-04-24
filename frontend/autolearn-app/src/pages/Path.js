f5cff2c24cabcffd424e0bf3d27fcbb708a1722f
t_tany <yikai.tan@autodesk.com>
2019-04-24 13:46:31 +0800

    added Home,Courses and Path page template

---
+++ frontend/autolearn-app/src/pages/Path.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
+
+export default class Path extends Component {
+    render() {
+        return (
+            <div>
+                Path
+            </div>
+        );
+    }
+}
+
+Path.propTypes = {
+    
+};
