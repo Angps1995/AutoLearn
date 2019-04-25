e033f4c7402dcb3c1f6cc9ca6af789e8243ecd76
t_tany <yikai.tan@autodesk.com>
2019-04-25 16:43:16 +0800

    partial link restructure

--- frontend/autolearn-app/src/pages/Courses.js
+++ frontend/autolearn-app/src/pages/Courses.js
@@ -17,17 +17,19 @@ export default class Courses extends Component {
         axios.get('http://localhost:5000/courses') // change this url to whichever end point to use
             .then(response => {
                 const myData = [].concat(response.data)
-                    .sort((a, b) => 
+                    .sort((a, b) =>
                         // console.log(a.votes>b.votes)
                         (a.votes < b.votes) ? 1 : ((b.votes < a.votes) ? -1 : 0)
-                        )
-                
+                    )
+
                 this.setState({
                     courses: myData
                 })
 
             })
     }
+
+
     render() {
         // const { courses } = this.state;
 
@@ -37,29 +39,34 @@ export default class Courses extends Component {
 
 
                 {this.state.courses.map((course, index) => {
+                    let tagsArr = course.tags.split(",");
                     return (
+
                         <Card>
                             <Card.Body>
                                 <span style={{ display: "inline-flex" }}>
-                                    <Button style={{ maxWidth: "100px", maxHeight: "60px", minWidth:"100px", minHeight:"30px", textAlign: "center", margin: "auto" }}>{course.votes}</Button>
+                                    <Button style={{ maxWidth: "100px", maxHeight: "60px", minWidth: "100px", minHeight: "30px", textAlign: "center", margin: "auto" }}>{course.votes}</Button>
                                     <span>
-                                        <Card.Title>{course.name}</Card.Title>
+                                        <Link to={{
+                                            pathname: '/path',
+                                            course_id: course.id
+                                        }}>
+                                            <Card.Title>{course.name}</Card.Title>
+                                            Path</Link>
                                         <Card.Text>{course.description}</Card.Text>
                                     </span>
                                 </span>
-                                <Row style={{ paddingLeft: "70px" }}>
-                                    {JSON.parse(course.tags).map((tag) => {
+                                <Row style={{ paddingLeft: "120px" }}>
+                                    {tagsArr.map((tag) => {
                                         return (
-                                            <Card.Text style={{ paddingRight: "10px" }}>{tag}</Card.Text>
+                                            <Card.Text style={{ paddingRight: "10px", boxSizing: "border-box" }}>{tag}</Card.Text>
                                         );
                                     })}
                                 </Row>
-                                <Link to={{
-                                    pathname: '/path',
-                                    course_id: course.id
-                                }}>Path</Link>
+
                             </Card.Body>
                         </Card>
+
                     );
                 })}
             </div>
