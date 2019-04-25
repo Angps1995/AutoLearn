43ae5e3d35ca4107357c39aa878f09c313877fcf
yeong jinzhi <yeong_jz@hotmail.com>
2019-04-25 11:19:28 +0800

    added some calling of api

--- frontend/autolearn-app/src/pages/Content.js
+++ frontend/autolearn-app/src/pages/Content.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
+import axios from 'axios';
 
 export default class Content extends Component {
-    render() {
-        return (
-            <div>
-                 Content
-            </div>
-        );
+    constructor () {
+      super()
+      this.state = {
+        username: ''
+      }
+      this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick () {
+       axios.get('http://localhost:5000/paths')
+         .then(response => this.setState({username: response.data[0].name}))
+
+     }
+
+    render () {
+      return (
+        <div className='button__container'>
+          <button className='button' onClick={this.handleClick}>
+            Click Me
+          </button>
+          <p>{this.state.username}</p>
+        </div>
+      )
     }
 }
+
+
 
 Content.propTypes = {
 
