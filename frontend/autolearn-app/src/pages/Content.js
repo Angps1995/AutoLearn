c405e838b95f03d74ee97ce38b76b4ade4614d1d
Fiona Leung <junmyeonstar@gmail.com>
2019-04-25 23:24:11 +0800

    changed subtitle text

--- frontend/autolearn-app/src/pages/Content.js
+++ frontend/autolearn-app/src/pages/Content.js
@@ -66,7 +66,7 @@ export default class Content extends Component {
             <Row className="Courses-subtitle-row">
               <Col>
                 <p className="text-align-center">
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type.
+                  Courses in this learning path, categorized by difficulty.
                 </p>
               </Col>
             </Row>
